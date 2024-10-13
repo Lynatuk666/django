02ca5991b6fb2e7a990d0e45b5e6a1af7bafdc32
Lynatuk666 <99319233+Lynatuk666@users.noreply.github.com>
2024-10-13 18:49:36 +0500

    прописал view фукнции

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 DATA = {
     'omlet': {
         'яйца, шт': 2,
@@ -15,16 +16,55 @@ DATA = {
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
+    }
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def home_view(request):
+    template_name = "calculator/home.html"
+    reciepts = {
+        "Невкусный омлет": reverse("omlet"),
+        "Мак & Чиз": reverse("pasta"),
+        "Бутерброд, со всяким": reverse("buter")
+    }
+    context = {
+        "pages": reciepts
+    }
+    return render(request, template_name, context)
+
+def omlet_view(request):  # если servings  нам не пришел в запросе, то он равняется 1
+    servings = 1  # тут хочется оптимизировать количество кода, но пока что так
+    if request.GET:  # мб вынести обработку ингридиентов в отдельную функцию
+        servings = int(request.GET["servings"])  # или можно переписать с использованием map()
+    template_name = 'calculator/index.html'
+    inf = {}
+    for key, value in DATA["omlet"].items():
+        inf[f'{key}'] = f"{value * servings}"
+    context = {"recipe": inf}
+    return render(request, template_name, context)
+
+
+def buter_view(request):
+    servings = 1
+    if request.GET:
+        servings = int(request.GET["servings"])
+    template_name = 'calculator/index.html'
+    inf = {}
+    for key, value in DATA["buter"].items():
+        inf[f'{key}'] = f"{value * servings}"
+    context = {"recipe": inf}
+    return render(request, template_name, context)
+
+
+def pasta_view(request):
+    servings = 1
+    if request.GET:
+        servings = int(request.GET["servings"])
+    template_name = 'calculator/index.html'
+    inf = {}
+    for key, value in DATA["pasta"].items():
+        inf[f'{key}'] = f"{value * servings}"
+    context = {"recipe": inf}
+
+    return render(request, template_name, context)
+
+
